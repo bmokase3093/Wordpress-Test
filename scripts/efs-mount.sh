2191b4bde3706de68c8f360a89f5e5b0a910014a
bmokase3093 <bmokase3093@gmail.com>
2022-11-22 16:27:57 +0200

    testing new

--- scripts/efs-mount.sh
+++ scripts/efs-mount.sh
@@ -8,9 +8,9 @@ sudo yum -y update
 sudo yum -y install nfs-utils
 
 
-mkdir /var/www/html/wordpress/efs
+mkdir /var/www/html/WordPress/efs
 
-sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $FS_ID:/   /var/www/html/wordpress/efs
+sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $FS_ID:/   /var/www/html/WordPress/efs
 # sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-07451f8ab8d7152f2.efs.us-east-1.amazonaws.com:/   ~/efs
 echo "${FS_ID}:/ /var/www/html/WordPress/efs nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0" >> /etc/fstab
 
