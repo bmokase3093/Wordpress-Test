5f805171235276ac4ba4237d7cc9f1b9fc88ebb9
bmokase3093 <bmokase3093@gmail.com>
2022-11-22 04:03:23 +0200

    Add large file

---
+++ wp-content/uploads/custom-css-js/1718.js
@@ -0,0 +1,14 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+  jQuery(".finish_course").click(function(){
+    console.log('hello user')
+      window.open('/feedback-form/','_blank');
+
+    });
+console.log('hello ')
+jQuery(document).on('click', '.finish_course', function (e) {
+    e.preventDefault();
+    window.open('/feedback-form/','_blank');
+
+});</script>
+<!-- end Simple Custom CSS and JS -->
